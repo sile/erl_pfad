9b3b04c6b16bd47c6f3da0f85a4dc59fec11cd5f
Takeru Ohta <phjgt308@gmail.com>
2014-03-23 23:53:29 +0900

    add pfad_ch14 module (for Chapter14: The last tail)

---
+++ src/pfad_ch14.erl
@@ -0,0 +1,35 @@
+%% @doc Chapter14: The last tail
+-module(pfad_ch14).
+
+-export([
+         maxtail/1,
+         linear_maxtail/1
+        ]).
+
+-spec maxtail([term()]) -> [term()].
+maxtail(List) ->
+    lists:max(tails(List)).
+
+-spec tails([term()]) -> [[term()]].
+tails([]) -> [];
+tails(Xs) -> [Xs | tails(tl(Xs))].
+
+-spec linear_maxtail([term()]) -> [term()].
+linear_maxtail([])       -> [];
+linear_maxtail([X | Xs]) -> step(0, 1, [X | Xs], [X | Xs], Xs).
+
+-spec step(non_neg_integer(), non_neg_integer(), [term()], [term()], [term()]) -> [term()].
+step(_, _, Ys, _, [])              -> Ys;
+step(P, Q, Ys, [W | Ws], [X | Xs]) ->
+    R = P rem Q,
+    if
+        W < X   -> linear_maxtail(lists:nthtail(Q - R, [W | Ws]));
+        W =:= X -> step(P + 1, Q, Ys, Ws, Xs);
+        W > X   -> step(0, P + Q + 1, Ys, Ys, Xs)
+    end.
+
+            
+
+
+
+
